050f4e49f84d1ade7b4e9912160bac167ffaaa10
LFer <lferreira@datamatic.com.uy>
2014-09-23 16:59:18 -0300

    Detalles minimos

--- visit.py
+++ visit.py
@@ -8,22 +8,15 @@ import time
 from osv import osv
 from osv import fields
 
-
-
 class visit(osv.osv):
     _name = "visit"
 
     _columns = {
-
-
 		'estate_id': fields.many2one('estate', 'Propiedad', required=True),
         'date': fields.date('Fecha', select=1, required=True),
         'partner_id': fields.many2one('res.partner', 'Visitante', required=True),
-        'notes': fields.text('Comentarios'),
-        
+        'notes': fields.text('Comentarios'),     
     }
-  
-    
 visit()
 
 
